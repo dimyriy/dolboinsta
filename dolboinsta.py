b61df961ca3ca883aa9d54e5d52c2b4ca98da181
dimyriy <dima.delmar@gmail.com>
2022-03-25 11:27:04 +0100

    Initialize browser explicitly

--- dolboinsta.py
+++ dolboinsta.py
@@ -1,15 +1,38 @@
 import os
-from instapy import InstaPy
 
-browser_executable_path = r'/usr/bin/firefox'
+from instapy import InstaPy
+from instapy.browser import create_firefox_extension, get_geckodriver
+from selenium import webdriver
+
+
+def browser_executable_path():
+    return r'/usr/bin/firefox'
+
+
+def browser():
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    options.binary_location = browser_executable_path()
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("permissions.default.image", 2)
+    profile.set_preference("media.volume_scale", "0.0")
+    profile.set_preference("dom.webdriver.enabled", False)
+    profile.set_preference("useAutomationExtension", False)
+    profile.set_preference("general.platform.override", "iPhone")
+    driver_path = get_geckodriver()
+    firefox = webdriver.Firefox(firefox_profile=profile, options=options, executable_path=driver_path)
+    firefox.install_addon(create_firefox_extension(), temporary=True)
+    return firefox
+
 
 insta_username = os.environ.get('insta_username')
 insta_password = os.environ.get('insta_password')
 
 session = InstaPy(username=insta_username,
                   password=insta_password,
-                  browser_executable_path=browser_executable_path,
+                  selenium_local_session=False,
                   headless_browser=True)
+session.browser = browser()
 session.login()
 
 followings = session.grab_following(insta_username, 1000)
@@ -19,4 +42,4 @@ followings_who_dont_follow = set(followings) - set(followers)
 
 for ff in followings_who_dont_follow:
     print(ff)
-exit(0)
+session.browser.quit()
