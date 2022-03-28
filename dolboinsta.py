2937a76e2f0a57a64622886ec0d39d7dc0491205
dimyriy <dima.delmar@gmail.com>
2022-03-28 11:22:27 +0200

    Some refactoring

--- dolboinsta.py
+++ dolboinsta.py
@@ -1,38 +1,48 @@
+#!/usr/bin/env python3
 import os
 
 from instapy import InstaPy
-from instapy.browser import create_firefox_extension, get_geckodriver
+from instapy.browser import create_firefox_extension
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
 def browser_executable_path():
-    return r'/usr/bin/firefox'
+    system_path = r'/usr/bin/firefox'
+    app_path = r'/Applications/Firefox.app/Contents/MacOS/firefox'
+    if os.path.exists(system_path):
+        return system_path
+    else:
+        if os.path.exists(app_path):
+            return app_path
+        else:
+            raise RuntimeError("Firefox executable not found in {0} and {1}".format(system_path, app_path))
 
 
-def browser():
+def browser(headless=True):
     options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    options.binary_location = browser_executable_path()
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("permissions.default.image", 2)
-    profile.set_preference("media.volume_scale", "0.0")
-    profile.set_preference("dom.webdriver.enabled", False)
-    profile.set_preference("useAutomationExtension", False)
-    profile.set_preference("general.platform.override", "iPhone")
-    driver_path = get_geckodriver()
-    firefox = webdriver.Firefox(firefox_profile=profile, options=options, executable_path=driver_path)
+    if headless:
+        options.add_argument('--headless')
+    options.binary = FirefoxBinary(firefox_path=browser_executable_path())
+    options.set_preference("permissions.default.image", 2)
+    options.set_preference("media.volume_scale", "0.0")
+    options.set_preference("dom.webdriver.enabled", False)
+    options.set_preference("useAutomationExtension", False)
+    options.set_preference("general.platform.override", "iPhone")
+    firefox = webdriver.Firefox(options=options)
     firefox.install_addon(create_firefox_extension(), temporary=True)
     return firefox
 
 
 insta_username = os.environ.get('insta_username')
 insta_password = os.environ.get('insta_password')
+headless_browser = False
 
 session = InstaPy(username=insta_username,
                   password=insta_password,
                   selenium_local_session=False,
-                  headless_browser=True)
-session.browser = browser()
+                  headless_browser=headless_browser)
+session.browser = browser(headless=headless_browser)
 session.login()
 
 followings = session.grab_following(insta_username, 1000)
