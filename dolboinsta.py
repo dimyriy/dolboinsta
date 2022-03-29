0bcd4d33dc538f6f89c377792b18a615315a7186
dimyriy <dima.delmar@gmail.com>
2022-03-29 11:23:11 +0200

    Pass service object to firefox constructor

--- dolboinsta.py
+++ dolboinsta.py
@@ -6,6 +6,7 @@ from instapy.browser import create_firefox_extension, get_geckodriver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -22,6 +23,15 @@ def browser_executable_path():
             raise RuntimeError("Firefox executable not found in {0} and {1}".format(system_path, app_path))
 
 
+def accept_gdpr(browser):
+    browser.get('https://instagram.com/accounts/login')
+    WebDriverWait(browser, 60).until(expected_conditions.visibility_of_element_located((By.ID, 'react-root')))
+    for element in browser.find_elements(by=By.TAG_NAME, value='button'):
+        if element.text.strip().lower() == 'allow essential and optional cookies':
+            element.click()
+            break
+
+
 def browser(headless=True):
     options = webdriver.FirefoxOptions()
     if headless:
@@ -32,28 +42,28 @@ def browser(headless=True):
     options.set_preference("dom.webdriver.enabled", False)
     options.set_preference("useAutomationExtension", False)
     options.set_preference("general.platform.override", "iPhone")
-    firefox = webdriver.Firefox(options=options, executable_path=get_geckodriver())
+
+    firefox = webdriver.Firefox(options=options, service=Service(executable_path=get_geckodriver()))
     firefox.set_script_timeout(60)
     firefox.implicitly_wait(60)
     firefox.install_addon(create_firefox_extension(), temporary=True)
-    firefox.get('https://instagram.com/accounts/login')
-    WebDriverWait(firefox, 60).until(expected_conditions.visibility_of_element_located((By.ID, 'react-root')))
-    for element in firefox.find_elements(by=By.TAG_NAME, value='button'):
-        if element.text.strip().lower() == 'allow essential and optional cookies':
-            element.click()
-            break
+
+    accept_gdpr(firefox)
+
     return firefox
 
 
+headless_browser = os.getenv('headless_browser', 'True').lower() in ('true', '1', 't')
 insta_username = os.environ.get('insta_username')
 insta_password = os.environ.get('insta_password')
-headless_browser = True
+if not insta_username or not insta_password:
+    raise RuntimeError('Environment variables insta_username and insta_password should be set')
 
 session = InstaPy(username=insta_username,
                   password=insta_password,
                   selenium_local_session=False,
                   headless_browser=headless_browser)
-session.browser = browser()
+session.browser = browser(headless_browser)
 session.login()
 
 followings = session.grab_following(insta_username, 1000)
